a8b66ff6306f96e24c5371b74701f6f86a9f13bb
csackrider <chadsackrider@hotmail.com>
2017-09-15 07:46:45 -0400

    removing android
    adding logging with log4j

--- src/test/java/stepDefs/Hooks.java
+++ src/test/java/stepDefs/Hooks.java
@@ -3,10 +3,8 @@ package stepDefs;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import helpers.baseClass;
-import io.selendroid.client.SelendroidDriver;
-import io.selendroid.common.SelendroidCapabilities;
-import io.selendroid.standalone.SelendroidConfiguration;
 import io.selendroid.standalone.SelendroidLauncher;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,7 +13,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
 public class Hooks extends baseClass {
-    static SelendroidLauncher selendroidServer=null;
+    static SelendroidLauncher selendroidServer = null;
+    final static Logger logger = Logger.getLogger(Hooks.class);
 
     @Before
     public void openBrowser() {
@@ -24,6 +23,7 @@ public class Hooks extends baseClass {
 
 
         String strBrowser = null;
+        boolean blnBrowserSupported = true;
         try {
             strBrowser = System.getProperty("BROWSER").toLowerCase();
         } catch (Exception e) {
@@ -45,31 +45,24 @@ public class Hooks extends baseClass {
                 cap.setBrowserName("firefox");
                 driver = new FirefoxDriver(cap);
                 break;
-            case "android":
-                SelendroidConfiguration config = new SelendroidConfiguration();
-                // Add the selendroid-test-app to the standalone server
-                config.addSupportedApp("src/main/resources/selendroid-test-app-0.17.0.apk");
-                selendroidServer = new SelendroidLauncher(config);
-                selendroidServer.launchSelendroid();
-                // Create the selendroid capabilities and specify to use an emulator and selendroid's test app
-                SelendroidCapabilities caps = new SelendroidCapabilities("io.selendroid.testapp:0.17.0");
-                try {
-                    driver = new SelendroidDriver(caps);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+            case "grid":
+                //driver = new RemoteWebDriver(new URL("https://turbo-seleniumgrid.hesos.net/wd/hub"), cap);
+
+                break;
+            default:
+                blnBrowserSupported = false;
+                logger.error("Browser '" + strBrowser + "' not supported yet.");
                 break;
         }
 
+        if (blnBrowserSupported) {
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
 
-        //for Grid
-        //driver = new RemoteWebDriver(new URL("https://turbo-seleniumgrid.hesos.net/wd/hub"), cap);
-
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().window().maximize(); // Always maximize firefox on windows
-
-
-        driver.get("http://web2.0calc.com/");
+            driver.get("http://web2.0calc.com/");
+        } else {
+            throw new IllegalStateException("Browser not supported!");
+        }
 
     }
 
